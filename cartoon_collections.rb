ea75ca354913e97980b53db651bd4c709e760a44
Miriam Castillo <castillo.m.mc@gmail.com>
2018-05-17 04:27:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ calls_short.any? do |i|
 end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese()# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
