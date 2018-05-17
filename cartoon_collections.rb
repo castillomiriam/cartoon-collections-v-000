e801858476e7a4d67cec32262eeea57fb9ad7436
Miriam Castillo <castillo.m.mc@gmail.com>
2018-05-17 03:43:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)# code an argument here
-  (roll_call_dwarves).each_with_index { |item, index|
+  array.each_with_index { |item, index|
     hash[item] = index
   # Your code here
 end
